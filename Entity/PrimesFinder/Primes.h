deb50dd67fb5cca3a1d2d9af38f38db24453882c
FraErriques <francesco.erriques@gmail.com>
2021-04-19 11:56:58 +0200

    monday fixes, on Entity::PrimesFined::Primes. TODO:lastRecordReader implementation.

--- Entity/PrimesFinder/Primes.h
+++ Entity/PrimesFinder/Primes.h
@@ -25,80 +25,47 @@ class Primes
 
     public:
     /// Data
-    /// Constructors and Destructors
-       Primes(// span upper_threshold-lastRecordOrdinal Integers, in default IntegralFile.
-              unsigned long upper_threshold
-         // ConfigFile for class Primes is "./PrimeConfig.txt"
-         // ConfigSectionName for standard-dump is assumed to be "PrimeIntegral_fromOrigin_" and dump-fullpath is in the ConfigSection.
-       );
-       Primes(); // default Ctor : span 100_Int, from lastRecord, in default IntegralFile.
-    Primes(
-        unsigned long lower_threshold,
-        unsigned long upper_threshold,
-        string & desiredConfigSectionName // SectionName in "./PrimeConfig.txt" for the desiderd file
-    );
-    // db-Ctor
-    Primes( string & DbConnectionStringConfigSectionName
-            ,string & tableName
-            ,unsigned long lower_threshold// only on custom intervals; no default table, on db.
-            ,unsigned long upper_threshold
-           );
-       // Destructor
-       ~Primes  ( void );
-       // some methoda
-       unsigned long getActualLength();
-       void LoggerSinkFS_example( unsigned long inf, unsigned long sup) const;
-       void IntegralFileFromStartFSproducer( unsigned long sup) const;
-       void IntegralFileFromAnywhereFSproducer( unsigned long inf, unsigned long sup) const;
-       void dumper();
-       unsigned long   operator[]  ( const unsigned long & requiredOrdinal )         const;// it's a read-only utility; syntax: Prime[ordinal]==...
+    /// Ctor
+    Primes();
+    ~Primes  ( void );
+    /// method
+    unsigned long getActualLength();
+    unsigned long getLastOrdinal();
+    unsigned long getLastPrime();
+    unsigned long operator[] (const unsigned long & requiredOrdinal) const;// it's a read-only utility; syntax: Prime[ordinal]==...
+    // to be tested:
+    const char * getDefaultPrimeDumpFullPath( const std::string & sectionNameInFile);
+    void  createOrAppend( const std::string & );
+    const char * lastRecordReader( const std::string & fullPath);
 
-        private:
-         /// Data
-         unsigned long ordinal;
-         unsigned long prime;
-         ofstream * appendStream = nullptr;// let it a ptr, so to annichilate it, as needed.
-         bool isHealthlyConstructed = false;
-//         unsigned ToBeDivided = 2;// start from two. One is invariant of the operator.
-//         unsigned threshold;
-//         unsigned primeOrdinal = 0L; // meglio non fare(after test). exTODO switch to System.Uunsigned everywhere. It leads from 9*10^18 to 18*10^18
 
-         unsigned long actualLength = 0L;
+   /// algo ///////////////////////////////////////////////////////////////////////
 
-//         unsigned desiredLength = 0L;
-//         unsigned actualOrdinal = 0L;
-//         unsigned desiredOrdinal = 0L;
-//         unsigned actualPrime = 0L;
-//         bool canOperate = false;
-        // Riemann exponent s in C; s=:(sigma + i*t).
-        double sigma;
-        double t;
-       /// copying methoda : not usable->private.
-       // Copy Constructor
-       Primes   ( const Primes & original );
-       // operator= only between isomorphic matrixes
-       Primes & operator=   ( const Primes & second );
-
-   /// algo ////////////////////////////////////////////////////////////////////////////////////////////////////
-   /*
-   StringMatrix   transpose   ( const StringMatrix & orig   )                    const;
-   StringMatrix   operator+   ( const StringMatrix & second )                    const;
-   bool           operator==  ( const StringMatrix & second )                    const;
-   void           reductor    ( size_t i, size_t j, StringMatrix & smaller )     const;
-   /// input-output ////////////////////////////////////////////////////////////////////////////////////////////
-   void show   (void)                                                            const;
-   bool insert (const std::string & what, const size_t row, const size_t col);   // cannot be const
-   */
 
 private:
-    const std::string & tokenEncoder( unsigned long ordinal, unsigned long prime ) const;
-    void lastRecordReader();// cannot be const: fills members.
+    /// Data
+    unsigned long lastOrdinal;
+    unsigned long lastPrime;
+    unsigned long desiredOrdinal;
+    unsigned long desiredPrime;
+    unsigned long actualPrimaryFileLength;
     const char * theDumpPath = nullptr;
-    //
-//   /// data ////////////////////////////////////////////////////////////////////////////////////////////
-//   bool readfile  ( const char * where );                                        // cannot be const; reads in RAM
-//   bool validate (const char * where)                                            const;
-//   bool writefile ( const char * where )                                         const;
+    bool isHealthlyConstructed = false;
+    bool canOperate = false;
+    // Riemann exponent s in C; s=:(sigma + i*t).
+    double sigma;
+    double t;
+    /// Ctor
+    // copying methoda : not usable->private.
+    // Copy Constructor
+    Primes   ( const Primes & original );
+    // operator= only between isomorphic matrixes
+    Primes & operator=   ( const Primes & second );
+    /// method
+    const std::string & tokenEncoder( unsigned long ordinal, unsigned long prime ) const;
+    void IntegralFileFromStartFSproducer( unsigned long sup) const;
+    void lastRecordReader();// cannot be const: fills members.
+
 };// class
 
 }// nmsp
