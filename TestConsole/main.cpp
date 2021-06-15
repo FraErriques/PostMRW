afefb19a3be9f8ddc8538cfb76b78db87abc734f
FraErriques <francesco.erriques@gmail.com>
2021-06-15 13:29:47 +0200

    a Factorization prototype in TestConsole::main.

--- TestConsole/main.cpp
+++ TestConsole/main.cpp
@@ -18,12 +18,34 @@
 
 
 
+void protoFactorize( unsigned long par)
+{
+    unsigned longsogliaStimata = par/2;// greatest involved-prime is the cofactor of the smallest one(i.e. 2).
+    // ordinaleStimato == LogIntegral[ sogliaStimata] NB. overstimate it, for safety.
+    unsigned ordinaleStimato = 56UL; // TODO
+    // TODO readRange( 1, ordinaleStimato);
+    unsigned long * involvedPrimes = new unsigned long[ordinaleStimato];
+    //..
+    delete[] involvedPrimes;
+}// protoFactorize
+
+
 int main()
 {// declare a threshold under the actual one. In case of accidental start, mothing will be done.
      PrimesFinder::Primes * p = new PrimesFinder::Primes(100);
-     unsigned long res = (*p)[25];
+     unsigned long res = (*p)[2];// TODO bug [1] #######################################################
+     unsigned long *pRange = new unsigned long[100];
+     for( int c=0; c<100; c++)
+     {
+         pRange[c] = (*p)[c+2];
+     }
+     for( int c=0; c<100; c++)
+     {
+         std::cout<<"\n\t Prime["<<c+2<<"]== "<<pRange[c];
+     }
      //...
      delete p;
+     delete[] pRange;
 
     //
     std::cout<<"\n\t Strike Enter to leave\t";
@@ -34,16 +56,13 @@ int main()
 
 /* cantina
 
-
-
-    //fReader_byRow();
-
 //     PrimesFinder::Primes * p = new PrimesFinder::Primes(4349999);
 //     unsigned long res = (*p)[3];
 //     res = (*p)[65123];
 //     res = (*p)[183123];
 //     res = (*p)[253000];
 //     res = (*p)[300001];
+//     delete p;
      //PrimesFinder::Primes * p = new PrimesFinder::Primes( 9888777, 9888999, "primeCustomFile");
      //p->Start_PrimeDump_FileSys();
 
