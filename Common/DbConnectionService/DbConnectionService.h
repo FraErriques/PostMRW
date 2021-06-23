80270feb8282063b54084859bc874d466ddc5601
fra <francesco.erriques@gmail.com>
2021-06-23 23:11:52 +0200

    build on June.23.2021 on Linux platform.

--- Common/DbConnectionService/DbConnectionService.h
+++ Common/DbConnectionService/DbConnectionService.h
@@ -13,6 +13,7 @@ namespace DbConnectionService
 
 
 //sql::ResultSet * dbName_tableName_LOAD_MULTI_( const std::string &dbName, const std::string &tableName);
+void do_usp_LOAD();
 int dbName_tableName_paramsList_INSERT_SINGLE( const std::string &dbName, const std::string &tableName, const std::string &paramsList );
 
 }// nmsp
