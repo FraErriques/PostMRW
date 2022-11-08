3d3d2e1b78f0f591cf574e943b8c741285db3155
FraErriques <francesco.erriques@gmail.com>
2022-11-08 17:26:30 +0100

    MonteCarlo work in progress.

--- Common/MonteCarlo/ClassicalDiscreteGenerator.cpp
+++ Common/MonteCarlo/ClassicalDiscreteGenerator.cpp
@@ -11,7 +11,7 @@
 #include <time.h>       /* time */
 #include <cmath>
 #include <iostream>
-#include "../StringBuilder/StringBuilder.h"
+//  #include "../StringBuilder/StringBuilder.h"
 #include "ClassicalDiscreteGenerator.h"
 
 namespace Common
@@ -40,25 +40,25 @@ ClassicalDiscreteGenerator::ClassicalDiscreteGenerator() : currentSeed( time(NUL
 
 
 void ClassicalDiscreteGenerator::resetExtractionInterval( int left, int right )
-         {
-            this->Min = left;// reset.
-            this->Sup = right;// reset.
-             this->theIntervalMeasure = right-left;// init for both models; correct only for discrete ones.
-             // default model is [min,sup) on [0,RAND_MAX)==[0,32767)
-             this->omothetia = this->theIntervalMeasure/((double)RAND_MAX);
-             this->translation = left;// NB. valid within a single population.
-             if( nullptr != this->discretePopulation)
-             {
-               delete this->discretePopulation;
-             }// else the population vector is already null.
-             this->discretePopulation = new std::vector<int>();// get a new population.
-             //
-             if( nullptr != this->frequencyDistribution)
-             {
-               delete this->frequencyDistribution;
-             }// else the frequencyDistribution vector is already null.
-             this->frequencyDistribution = new std::vector<DeltaOmega>();// get a new frequencyDistribution.
-         }// ex_Ctor
+{//NB. for DiscreteGenerator the domain is a compact [left,right]
+    this->Min = left;// reset.
+    this->Sup = right;// reset.
+    this->theIntervalMeasure = right-left+1;// init for Discrete models: measure[+1,+3]==+3=={1,2,3}==right-left+1
+    // default model is [min,sup) on [0,RAND_MAX)==[0,32767)
+    this->omothetia = this->theIntervalMeasure/((double)RAND_MAX);
+    this->translation = left;// NB. valid within a single population.
+    if( nullptr != this->discretePopulation)
+    {
+        delete this->discretePopulation;
+    }// else the population vector is already null.
+    this->discretePopulation = new std::vector<int>();// get a new population.
+    //
+    if( nullptr != this->frequencyDistribution)
+    {
+        delete this->frequencyDistribution;
+    }// else the frequencyDistribution vector is already null.
+    this->frequencyDistribution = new std::vector<DeltaOmega>();// get a new frequencyDistribution.
+}// ex_Ctor
 
 
         void ClassicalDiscreteGenerator::nextIntegerInInterval() const
@@ -146,26 +146,26 @@ void ClassicalDiscreteGenerator::resetExtractionInterval( int left, int right )
 
         void ClassicalDiscreteGenerator::buildOmega( int partizioneLeft ,int partizioneRight)
         {//this->frequencyDistribution has been built by Ctor.
-            for( double position=partizioneLeft; position<partizioneRight; position++)
+            for( double position=partizioneLeft; position<=partizioneRight; position++)
             {
                 DeltaOmega * curDeltaOmega = new DeltaOmega( position, +1E-80);// TODO verify
                 this->frequencyDistribution->push_back( *curDeltaOmega );
             }//for
         }//buildOmega
 
-void ClassicalDiscreteGenerator::buildOmega( double partizioneLeft, double partizioneRight)
-{//this->frequencyDistribution has been built by Ctor.
-    double eta = (partizioneRight-partizioneLeft)/20.0;//each DeltaOmega is 1/10*Omega. Eta is 1/2*DeltaOmega.
-    if(eta<+0.5){eta=+1.0/+2.0;}
-    double mediana = 0;// init
-    for( double position=partizioneLeft-+1.0E-80; mediana<partizioneRight; position+=2.0*eta)
-    {
-        mediana = position+eta;
-        if(mediana>=partizioneRight){break;}
-        DeltaOmega * curDeltaOmega = new DeltaOmega( mediana, eta);// TODO verify
-        this->frequencyDistribution->push_back( *curDeltaOmega );
-    }//for
-}//buildOmega
+//void ClassicalDiscreteGenerator::buildOmega( double partizioneLeft, double partizioneRight)
+//{//this->frequencyDistribution has been built by Ctor.
+//    double eta = (partizioneRight-partizioneLeft)/20.0;//each DeltaOmega is 1/10*Omega. Eta is 1/2*DeltaOmega.
+//    if(eta<+0.5){eta=+1.0/+2.0;}
+//    double mediana = 0;// init
+//    for( double position=partizioneLeft-+1.0E-80; mediana<partizioneRight; position+=2.0*eta)
+//    {
+//        mediana = position+eta;
+//        if(mediana>=partizioneRight){break;}
+//        DeltaOmega * curDeltaOmega = new DeltaOmega( mediana, eta);// TODO verify
+//        this->frequencyDistribution->push_back( *curDeltaOmega );
+//    }//for
+//}//buildOmega
 
 
 unsigned int ClassicalDiscreteGenerator::showCurrentSeed() const
